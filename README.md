1c938acf323e6b2b2ed1e50854c003b58bc74ad4
Eslam Mohammed Saad <eslamooo5522@outlook.com>
2024-08-24 02:55:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# dalel
+
+My Second Flutter project.
+
+## Getting Started
+
+New Things I will cover in this project:
+- Clean Architecture
+- Firebase Services
+- splash screen
+- Enhance my UI/UX skills
+
+
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DalelApp());
+}
+
+class DalelApp extends StatelessWidget {
+  const DalelApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+
+    );
+  }
+}
+
